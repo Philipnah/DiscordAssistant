f539d891d81d5d972e0a63d70a6b38b325b992ab
Philip Han <46556419+Philipnah@users.noreply.github.com>
2021-08-08 17:59:04 +0200

    Added main script and changed gitignore

---
+++ main.py
@@ -0,0 +1,56 @@
+import requests
+
+# Set this to your path to Discord
+path2DiscordToken = r"C:\Users\phili\AppData\Local\Discord\token.txt"
+
+message = "cs?"
+
+# the names of people you have put in your contacts
+contactList = ["Fredrik", "Dyrving", "Morten"]
+
+def GetToken(path):
+
+	# Follow read.me guide for getting your authorization token set up
+	with open(path, 'r') as f:
+		lines = f.readlines()
+		
+		for line in lines:
+			if "Token:" in line:
+				authToken = line[7:]
+
+	return authToken
+
+def GetChannelIDs():
+	with open("contacts.txt", "r") as f:
+		receivers = f.readlines()
+
+	contactIDs = []
+
+	for receiver in receivers:
+		for name in contactList:
+			if name in receiver:
+
+				# -19 removes everything but the ID and \n, -1 removes \n
+				contactIDs.append(receiver[-19:-1])
+				
+	return contactIDs
+
+
+token = GetToken(path2DiscordToken)
+contactIDs = GetChannelIDs()
+
+print(contactIDs)
+
+payload = {
+	"content": message
+}
+
+header = {
+	"authorization": token
+}
+
+
+	
+for id in contactIDs:
+	r = requests.post("https://discord.com/api/v9/channels/" + id + "/messages", data=payload, headers=header)
+
